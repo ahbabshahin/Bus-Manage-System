ee22be46580cdfe03be554e1f8913b9503726be1
Ahbab <ahbabshahin7788@gmail.com>
2023-02-28 13:43:44 +0600

    Inventory Schema

---
+++ models/BusInventory.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const InvestorySchema = new mongoose.Schema({
+    licenseNumber:{
+        type:String,
+        required: [true, 'must be given'],
+    },
+    codeName: {
+        type: String,
+        // unique: true,
+        required: true,
+    },
+    capacity:{
+        type: Number,
+        required:true,
+    },
+
+    driverInfo :{
+        name:{
+            type: String,
+            required:[true, 'must provide full name'],
+        },
+        contactNumber:{
+            type: Number,
+            required:true,
+        },
+    },
+    isActive:{
+        type: Boolean,
+        default: false,
+    }
+});
+
+module.exports = mongoose.model('Inventory',InvestorySchema);
